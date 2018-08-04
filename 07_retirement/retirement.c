40c5601768ea62d017e9c2fcd3f5766232eebd90
Coursera Learner <nobody@nowhere.nul>
2018-08-04 08:41:28 +0000

    struct proble fixed

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct _retire_info{
+struct _retire_info{
   int months;
   double contribution;
   double rate_of_return;
-}retire_info;
+}
+  typedef struct  _retire_info retire_info;
 
 void retirement(int startAge, double initial, retire_info working, retire_info retired){
   int month=3;
