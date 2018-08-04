665359b59a44e2edf83ed91dfe2901e28f868ff7
Coursera Learner <nobody@nowhere.nul>
2018-08-04 08:32:23 +0000

    Did assigenmet 7

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _retire_info{
+  int months;
+  double contribution;
+  double rate_of_return;
+}retire_info;
+
+void retirement(int startAge, double initial, retire_info working, retire_info retired){
+  int month=3;
+  for(int i = 0; i < working.months; i++){
+    if(month==12){
+      month=0;
+      startAge++;
+    }
+    printf("Age %3d month %2d you have $%.2lf\n", startAge, month, initial);
+    initial += initial*working.rate_of_return+working.contribution;
+    month++;
+  }
+  for(int i = 0; i < retired.months; i++){
+    if(month==12){
+      month=0;
+      startAge++;
+    }
+    printf("Age %3d month %2d you have $%.2lf\n", startAge, month, initial);
+    initial += initial*retired.rate_of_return+retired.contribution;
+    month++;
+  }
+}
+
+int main(){
+  retire_info working, retired;
+  working.months=489;
+  working.contribution=1000;
+  working.rate_of_return=0.045/12;
+  retired.months=384;
+  retired.contribution=-4000;
+  retired.rate_of_return=0.01/12;
+  retirement(27, 21345, working, retired);
+}
+  
